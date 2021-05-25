23c4202548cb93f30a3c70d6f3f7a980b19c5d33
caifh <caifh@txxy.com>
2021-05-25 12:45:13 +0800

    1.优化实现逻辑。

--- common/common_test.go
+++ common/common_test.go
@@ -1,2 +1,14 @@
-// common_test.go
 package common
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRunFuncName(t *testing.T) {
+
+	name := RunFuncName()
+	expected := `github.com/cfh0081/golib/common.TestRunFuncName`
+	assert.Equal(t, name, expected)
+}
